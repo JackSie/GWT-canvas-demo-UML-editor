9a1eab41b88e983b06e01728a6bcd40c073c74a3
Jack Sie <jetswayss@gmail.com>
2014-07-05 20:06:39 +0800

    use buffered image

--- src/main/java/org/twbbs/peak/canvastest/client/HelloWorldE.java
+++ src/main/java/org/twbbs/peak/canvastest/client/HelloWorldE.java
@@ -34,27 +34,36 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class HelloWorldE implements EntryPoint {
 	public static int refreshRate = 25;
 	 Context2d context;
+	 Context2d bufferedContext;
 	 GraphicCenter<Context2d> graphicCenter;
 	 Label greetingwords;
 	 UmlClass umlClass;
+	 Canvas canvas;
+	 Canvas bufferedCanvas;
 	public void onModuleLoad() {
 		
 		greetingwords = new Label("hello Canvas");
 		RootPanel.get().add(greetingwords);
-		Canvas canvas = Canvas.createIfSupported();
+		canvas = Canvas.createIfSupported();
+		bufferedCanvas=Canvas.createIfSupported();
 		if(canvas==null){
 			Label errormsg = new Label("canvas not supported");
 			RootPanel.get().add(errormsg);
 			return;
 		}
+		
+		
+		
 		canvas.setWidth("1024px");
 		canvas.setHeight("768px");
 		canvas.setCoordinateSpaceWidth(1024);
+		bufferedCanvas.setCoordinateSpaceWidth(1024);
 		canvas.setCoordinateSpaceHeight(768);
+		bufferedCanvas.setCoordinateSpaceHeight(768);
 		
 		RootPanel.get().add(canvas);
 		context=canvas.getContext2d();
-	
+		bufferedContext=bufferedCanvas.getContext2d();
 		
 		Image image = new Image("images/ab.png");
 		final ImageElement im = ImageElement.as(image.getElement());
@@ -101,8 +110,10 @@ public class HelloWorldE implements EntryPoint {
 	}
 	
 	void doUpdate(){
-		
-		graphicCenter.draw(context);
+		bufferedContext.setFillStyle("#eee");
+		bufferedContext.fillRect(0, 0, 1024, 768);
+		graphicCenter.draw(bufferedContext);
+		context.drawImage(bufferedContext.getCanvas(), 0, 0);
 	}
 	
 	private void initHandler(Canvas canvas){
