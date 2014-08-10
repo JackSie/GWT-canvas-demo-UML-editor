c2f8592b29d7ddcd3dd793883a75f90e2078c838
Jack Sie <jetswayss@gmail.com>
2014-08-10 20:38:44 +0800

    refact ConnectionManager

--- src/main/java/org/twbbs/peak/uml/manage/connection/UMLConnectionManagerImpl.java
+++ src/main/java/org/twbbs/peak/uml/manage/connection/UMLConnectionManagerImpl.java
@@ -6,23 +6,24 @@ import org.twbbs.peak.uml.object.basic.UMLBasicObject;
 
 public class UMLConnectionManagerImpl implements UMLConnectionManager{
     protected UMLObjectManager manager;
-    protected UMLBasicObject nowObject;
-    protected UMLBasicObject object;
+    protected UMLBasicObject preObject;
+    protected UMLBasicObject postObject;
     public UMLConnectionManagerImpl(UMLObjectManager manager) {
         this.manager=manager;
     }
+    
     public void connectStart(int x, int y) {
         UMLObject object=manager.getUMLObject(x, y);
         if(object!=null && object instanceof UMLBasicObject){
-            nowObject=(UMLBasicObject)object;
+            preObject=(UMLBasicObject)object;
         }
     }
     public boolean connectEnd(int x, int y) {
-        object=null;
-        if(nowObject!=null){
+        postObject=null;
+        if(preObject!=null){
             UMLObject umlObject=manager.getUMLObject(x, y);
             if(umlObject!=null && umlObject instanceof UMLBasicObject){
-                object=(UMLBasicObject)umlObject;
+                postObject=(UMLBasicObject)umlObject;
             }
             if(isBothObjectsExist()){
                 return true;
@@ -32,28 +33,28 @@ public class UMLConnectionManagerImpl implements UMLConnectionManager{
     }
     public boolean associateObjects() {
         if(isBothObjectsExist()){
-            manager.associateObjects(nowObject,object);
+            manager.associateObjects(preObject,postObject);
         }
         return initWithPreStatement();
     }
     public boolean compositeObjects() {
         if(isBothObjectsExist()){
-            manager.compositeObjects(nowObject,object);
+            manager.compositeObjects(preObject,postObject);
         }
         return initWithPreStatement();
     }
     public boolean generalizeObjects() {
         if(isBothObjectsExist()){
-            manager.generalizeObjects(nowObject,object);
+            manager.generalizeObjects(preObject,postObject);
         }
         return initWithPreStatement();
     }
     private boolean isBothObjectsExist(){
-        return object!=null && nowObject!=object;
+        return postObject!=null && preObject!=postObject;
     }
     private void init(){
-        nowObject=null;
-        object=null;
+        preObject=null;
+        postObject=null;
     }
     private boolean initWithPreStatement(){
         boolean result=false;
