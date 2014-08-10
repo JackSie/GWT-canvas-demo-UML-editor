1fb46fc70be5132ee4e61ba06eff7cdc6cc9311a
Jack Sie <jetswayss@gmail.com>
2014-08-10 20:31:35 +0800

    1.0.0-SNAPSHOT

--- src/test/java/org/twbbs/peak/uml/object/basic/detail/UMLDetailsAllTest.java
+++ src/test/java/org/twbbs/peak/uml/object/basic/detail/UMLDetailsAllTest.java
@@ -3,34 +3,52 @@ package org.twbbs.peak.uml.object.basic.detail;
 import com.google.gwt.junit.client.GWTTestCase;
 
 public class UMLDetailsAllTest extends GWTTestCase{
-	public void testUMLDetailMember(){
-		UMLDetailMember member=new UMLDetailMember();
-		String name="haha";
-		String type="String";
-		int scope=3;
-		member.setName(name);
-		member.setType(type);
-		member.setScope(scope);
-		assertEquals(name, member.getName());
-		assertEquals(type, member.getType());
-		assertEquals(scope, member.getScope());
+	public void testUMLDetailMemberName(){
+	    UMLDetailMember member=new UMLDetailMember();
+        String name="haha";        
+        member.setName(name);       
+        assertEquals(name, member.getName());
 	}
-	public void testUMLDetailMethod(){
-		UMLDetailMethod method=new UMLDetailMethod();
-		String name="haha";
-		String type="String";
-		String parameters[]={"Integer","String"};
-		int scope=3;
-		method.setName(name);
-		method.setType(type);
-		method.setScope(scope);
-		method.setParameters(parameters);
-		assertEquals(name, method.getName());
-		assertEquals(type, method.getType());
-		assertEquals(scope, method.getScope());
-		assertEquals(parameters[0], method.getParameters()[0]);
-		assertEquals(parameters[1], method.getParameters()[1]);
+	public void testUMLDetailMemberType(){
+	    UMLDetailMember member=new UMLDetailMember();
+        String type="String";
+        member.setType(type);
+        assertEquals(type, member.getType());
 	}
+	public void testUMLDetailMemberScope(){
+	    UMLDetailMember member=new UMLDetailMember();        
+        int scope=3;
+        member.setScope(scope);
+        assertEquals(scope, member.getScope());
+	}
+
+    public void testUMLDetailMethodName() {
+        UMLDetailMethod method = new UMLDetailMethod();
+        String name = "haha";
+        method.setName(name);
+        assertEquals(name, method.getName());
+    }
+
+    public void testUMLDetailMethodType() {
+        UMLDetailMethod method = new UMLDetailMethod();
+        String type = "String";
+        method.setType(type);
+        assertEquals(type, method.getType());
+    }
+
+    public void testUMLDetailMethodScope() {
+        UMLDetailMethod method = new UMLDetailMethod();
+        int scope = 3;
+        method.setScope(scope);
+        assertEquals(scope, method.getScope());
+    }
+
+    public void testUMLDetailMethodParameter() {
+        UMLDetailMethod method = new UMLDetailMethod();
+        String parameters[] = { "Integer", "String" };
+        method.setParameters(parameters);
+        assertEquals(parameters[0], method.getParameters()[0]);
+    }
 	@Override
 	public String getModuleName() {
 		return "org.twbbs.peak.umlJUnit";
